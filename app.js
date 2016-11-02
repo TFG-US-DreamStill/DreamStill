cf14a706032952a8c0aa8c30c5a67b2b2268f997
juanrarodriguez18 <dragondr3w@gmail.com>
2016-11-02 20:53:27 +0100

    Añadido inicio para la aplicación Node con App.js e instalación del módulo imap para leer correos de Gmail

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(__dirname));
+app.use(express.cookieParser('cookies monster')); // Cookie secret
+
+// development only
+if ('development' == app.get('env')) {
+app.use(express.errorHandler());
+}
+
+app.use(function(req, res) {
+  // Use res.sendfile, as it streams instead of reading the file into memory.
+  res.sendfile(__dirname + '/index.html');
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
