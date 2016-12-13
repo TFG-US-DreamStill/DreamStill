483eefb267a96999c9f8450563b3c02bc52fa297
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-13 08:55:09 +0100

    Comiteada primera Gráfica del componente de representación de graficas

--- app/graphs.component.ts
+++ app/graphs.component.ts
@@ -9,6 +9,14 @@ import { FirebaseService }          from './firebase.service';
 })
 export class GraphsComponent implements OnInit {
     datos: JSON;
+    public message: string;
+    private sub: any;
+    public dreamDataX: String[];
+    public dreamDataY: String[];
+    private name: string;
+    public PlotlyLayout: any;
+    public PlotlyData: any;
+    public PlotlyOptions: any;
 
     constructor(private _firebaseService: FirebaseService, private route: ActivatedRoute) { }
 
@@ -17,17 +25,45 @@ export class GraphsComponent implements OnInit {
             if (params['app'] !== undefined && params['date'] !== undefined) {
                 if (params['app'] === 'morpheuz'){
                     this._firebaseService.getMorpheuzDataOfUserAtDate(new Date(params['date'])).subscribe(
-                                                                        info => console.log(JSON.stringify(info)),
+                                                                        data => this.setReturnedData(data),
                                                                         error => console.log(error))
                 }
-                /*let id = +params['id'];
-                this.navigated = true;
-                this.heroService.getHero(id)
-                    .then(hero => this.hero = hero);*/ 
             } else {
                /* this.navigated = false; 
                 this.hero = new Hero();*/
             }
         });
+     }
+
+    private setReturnedData(data: any) {
+        console.log(data);
+        this.dreamDataX = [];
+        this.dreamDataY = [];
+        for (let d of data){
+            console.log("d:"+d);
+            this.dreamDataX.push(d.Hour);
+            this.dreamDataY.push(d.Movements);
+        }
+        /*for (let d in data){
+            this.dreamDataY.push(d.Movements);
+        }*/
+        this.PlotlyLayout = {
+            title: "Numero de movimientos",
+            height: 500,
+            width: 1800
+        };
+
+        this.PlotlyData = [
+            {
+                x: this.dreamDataX,
+                y: this.dreamDataY,
+                name: "Number of snake bite deaths",
+                type: 'scatter', // set the chart type
+                mode: 'lines' // connect points with lines
+            }
+        ];
+
+        console.log("recieved plotly data");
+        console.log(this.PlotlyData);
   }
 }
