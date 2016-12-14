1eeb1950608162b1b7e257e5e1f3cfe7c9bf4f8e
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-14 19:21:22 +0100

    Añadido marcar como leido

--- emailReader.js
+++ emailReader.js
@@ -22,52 +22,58 @@ imap.once('ready', function() {
   openInbox(function(err, box) {
   if (err) throw err;
   imap.search([ 'UNSEEN', ['FROM', 'noreply@morpheuz.co.uk'] ], function(err, results) {
+    try{
     var f = imap.fetch(results,  {
         bodies: ['HEADER.FIELDS (TO SUBJECT)','TEXT'],
       struct: true,
       markSeen: true
     });
-    f.on('message', function(msg, seqno) {
-      console.log('Message #%d', seqno);
-      var prefix = '(#' + seqno + ') ';
-      var buffer = '';
-      msg.on('body', function(stream, info) {
-        console.log(prefix + 'Body');
-        //stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
-        stream.on('data', function(chunk) {
-            buffer += chunk.toString('utf8');
-              if(buffer.includes("To:")){
-                const regexBody = /<html>(.*)<\/html>/;
-                const regexTo = /To: (.*)m/;
-                const regexSubject = /Subject: (.*)/;
-                //console.log("BUFFER", buffer);
-                buffer = buffer.replace(/(\r\n|\n|\r)/gm,"");
-                /*console.log("TO: "+String(regexTo.exec(buffer)[0]).replace(/To: /,""))
-                console.log("SUBJECT: "+String(regexSubject.exec(buffer)[0]).replace(/Subject: /,""))
-                console.log("BODY: "+regexBody.exec(buffer))*/
-                var to = String(regexTo.exec(buffer)[0]).replace(/To: /,"");
-                var subject = String(regexSubject.exec(buffer)[0]).replace(/Subject: /,"");
-                var body = String(regexBody.exec(buffer));
-                console.log(email2Json.parseEmail2Json(to, subject, body));
+    }catch(Exception){
 
-                var data = email2Json.parseEmail2Json(to, subject, body)[0];
-                var user = email2Json.parseEmail2Json(to, subject, body)[1];
+    }
+    if(f!==undefined){
+      f.on('message', function(msg, seqno) {
+        console.log('Message #%d', seqno);
+        var prefix = '(#' + seqno + ') ';
+        var buffer = '';
+        msg.on('body', function(stream, info) {
+          console.log(prefix + 'Body');
+          //stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+          stream.on('data', function(chunk) {
+              buffer += chunk.toString('utf8');
+                if(buffer.includes("To:")){
+                  const regexBody = /<html>(.*)<\/html>/;
+                  const regexTo = /To: (.*)m/;
+                  const regexSubject = /Subject: (.*)/;
+                  //console.log("BUFFER", buffer);
+                  buffer = buffer.replace(/(\r\n|\n|\r)/gm,"");
+                  /*console.log("TO: "+String(regexTo.exec(buffer)[0]).replace(/To: /,""))
+                  console.log("SUBJECT: "+String(regexSubject.exec(buffer)[0]).replace(/Subject: /,""))
+                  console.log("BODY: "+regexBody.exec(buffer))*/
+                  var to = String(regexTo.exec(buffer)[0]).replace(/To: /,"");
+                  var subject = String(regexSubject.exec(buffer)[0]).replace(/Subject: /,"");
+                  var body = String(regexBody.exec(buffer));
+                  console.log(email2Json.parseEmail2Json(to, subject, body));
 
-                firebaseApi.setMorpheuzUserData(data, user);
-              }
-          });
+                  var data = email2Json.parseEmail2Json(to, subject, body)[0];
+                  var user = email2Json.parseEmail2Json(to, subject, body)[1];
+
+                  firebaseApi.setMorpheuzUserData(data, user);
+                }
+            });
+        });
+        msg.once('end', function() {
+          console.log(prefix + 'Finished');
+        });
       });
-      msg.once('end', function() {
-        console.log(prefix + 'Finished');
+      f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
       });
-    });
-    f.once('error', function(err) {
-      console.log('Fetch error: ' + err);
-    });
-    f.once('end', function() {
-      console.log('Done fetching all messages!');
-      imap.end();
-    });
+      f.once('end', function() {
+        console.log('Done fetching all messages!');
+        imap.end();
+      });
+    }
   });
 });
 });
