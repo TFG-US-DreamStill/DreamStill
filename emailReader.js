266c4f82d62079eee2d1a65c9138fd9da055619f
juanrarodriguez18 <dragondr3w@gmail.com>
2016-12-14 17:33:55 +0100

    Añadido el almacenamiento de correos de Morpheuz pendientes al iniciar el servidor, a falta de marcar los emails como leidos

--- emailReader.js
+++ emailReader.js
@@ -1,13 +1,7 @@
 require('dotenv').config()
-//var fs, passwordFile;
 
-/*passwordFile = 'passwords.json';
-fs = require('fs');
-
-var configuration = JSON.parse(
-    fs.readFileSync(passwordFile)
-);*/
-
+var email2Json = require('./email2Json.js');
+var firebaseApi = require('./firebase.api.js');
 var Imap = require('imap'),
     inspect = require('util').inspect;
 
@@ -26,28 +20,46 @@ function openInbox(cb) {
 
 imap.once('ready', function() {
   openInbox(function(err, box) {
-    if (err) throw err;
-    var f = imap.seq.fetch('6:8', {
-        bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)','TEXT'],
-      struct: true
+  if (err) throw err;
+  imap.search([ 'UNSEEN', ['FROM', 'noreply@morpheuz.co.uk'] ], function(err, results) {
+    var f = imap.fetch(results,  {
+        bodies: ['HEADER.FIELDS (TO SUBJECT)','TEXT'],
+      struct: true,
+      markSeen: true
     });
     f.on('message', function(msg, seqno) {
-        console.log('Message #%d', seqno);
-        var prefix = '(#' + seqno + ') ';
-        msg.on('body', function(stream, info) {
-          if (info.which === 'TEXT')
-          var buffer = '', count = 0;
-          stream.on('data', function(chunk) {
-            count += chunk.length;
+      console.log('Message #%d', seqno);
+      var prefix = '(#' + seqno + ') ';
+      var buffer = '';
+      msg.on('body', function(stream, info) {
+        console.log(prefix + 'Body');
+        //stream.pipe(fs.createWriteStream('msg-' + seqno + '-body.txt'));
+        stream.on('data', function(chunk) {
             buffer += chunk.toString('utf8');
+              if(buffer.includes("To:")){
+                const regexBody = /<html>(.*)<\/html>/;
+                const regexTo = /To: (.*)m/;
+                const regexSubject = /Subject: (.*)/;
+                //console.log("BUFFER", buffer);
+                buffer = buffer.replace(/(\r\n|\n|\r)/gm,"");
+                /*console.log("TO: "+String(regexTo.exec(buffer)[0]).replace(/To: /,""))
+                console.log("SUBJECT: "+String(regexSubject.exec(buffer)[0]).replace(/Subject: /,""))
+                console.log("BODY: "+regexBody.exec(buffer))*/
+                var to = String(regexTo.exec(buffer)[0]).replace(/To: /,"");
+                var subject = String(regexSubject.exec(buffer)[0]).replace(/Subject: /,"");
+                var body = String(regexBody.exec(buffer));
+                console.log(email2Json.parseEmail2Json(to, subject, body));
 
-            console.log("BUFFER", buffer);
+                var data = email2Json.parseEmail2Json(to, subject, body)[0];
+                var user = email2Json.parseEmail2Json(to, subject, body)[1];
+
+                firebaseApi.setMorpheuzUserData(data, user);
+              }
           });
-          stream.once('end', function() {
-            if (info.which !== 'TEXT')
-              console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
-          });
-        });
+      });
+      msg.once('end', function() {
+        console.log(prefix + 'Finished');
+      });
     });
     f.once('error', function(err) {
       console.log('Fetch error: ' + err);
@@ -57,6 +69,7 @@ imap.once('ready', function() {
       imap.end();
     });
   });
+});
 });
 
 imap.once('error', function(err) {
