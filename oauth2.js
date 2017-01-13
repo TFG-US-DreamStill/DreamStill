7e5ba113047f5e34f4581f56bc089e0b1aa059d7
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-13 11:11:40 +0100

    Terminados los métodos necesarios para la comprobación y refresco del token de GoogleFit

--- oauth2.js
+++ oauth2.js
@@ -73,6 +73,7 @@ module.exports = {
         }
       } else {
         console.log('Done!')
+        console.log(body)
       }
     });
   },
@@ -106,28 +107,6 @@ module.exports = {
   }
 }
 
-function googleFitCheckToken(username, access_token, refresh_token) {
-    //https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=access_token
-    requestA({
-      url: 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + access_token,
-      method: 'GET'
-    }, function (error, response, body) {
-      if (error) {
-        console.error(error, response, body);
-      }
-      else if (response.statusCode >= 400) {
-        console.error('HTTP Error: ' + response.statusCode + ' - ' + response.statusMessage + '\n' + body);
-        errorCause = JSON.parse(body)["error"];
-        console.log(errorCause);
-        if (errorCause == "invalid_token") {
-          googleFitRefreshToken(username, refresh_token);
-        }
-      } else {
-        console.log('Done!')
-      }
-    });
-  }
-
 function googleFitRefreshToken(username, refresh_token) {
   requestA({
       url: 'https://www.googleapis.com/oauth2/v4/token',
@@ -135,7 +114,7 @@ function googleFitRefreshToken(username, refresh_token) {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: 'client_id=930495267346-vm7uumk0hjfmvb8rd3pre2tf5qn177m3.apps.googleusercontent.com&client_secret=' + process.env.GOOGLE_CLIENT_SECRET + '&refresh_token=' + refresh_token + '&grant_type=refresh_token&access_type=offline'
+      body: 'client_id=' + process.env.GOOGLE_CLIENT_ID + '&client_secret=' + process.env.GOOGLE_CLIENT_SECRET + '&refresh_token=' + refresh_token + '&grant_type=refresh_token&access_type=offline'
     }, function (error, response, body) {
       if (error) {
         console.error(error, response, body);
@@ -149,12 +128,9 @@ function googleFitRefreshToken(username, refresh_token) {
         //console.log(body)
         console.log(JSON.parse(body)["access_token"])
         firebaseAPI.setGoogleTokenToUser(username, JSON.parse(body)["access_token"], refresh_token)
-        res.redirect("/")
       }
     });
 }
 
 function fitbitRefreshToken(username, refresh_token) {
 }
-
-googleFitCheckToken("juanra", "ya28.Ci_SA2Q8jHXODLiH4Mam6piEYO4ydNnObyJsfErCEQNEBk5jgusCShav_Iw82TBZjg","1/aq2INVwqlPaDRYLCdfMvonM2-kbjtjd7Z22HDSbl-VcuuN_dUJ_NSzX3QHEgUzr")
