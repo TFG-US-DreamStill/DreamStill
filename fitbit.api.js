955d6500cfc75523bcb4ba92d690ba4421ff225a
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-15 20:43:35 +0100

    Añadida base de método para obtener los días con sueño a partir de una fecha

--- fitbit.api.js
+++ fitbit.api.js
@@ -2,4 +2,32 @@ require('dotenv').config()
 const firebaseAPI = require('./firebase.api.js');
 var requestA = require('request');
 
-module.exports = { }
+module.exports = {
+
+    getDaysWithSleepFromDate: function (user_id, access_token, date) {
+        var year = "" + date.getFullYear();
+        var month = ("0" + date.getMonth() + 1).slice(-2);
+        var day = ("0" + date.getDate()).slice(-2);
+        date_from = year + '-' + month + '-' + day;
+        requestA({
+            url: 'https://api.fitbit.com/1/user/' + user_id + '/sleep/timeInBed/date/' + date_from + '/today.json',
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + access_token
+            },
+        }, function (error, response, body) {
+            if (error) {
+                console.error(error, response, body);
+            }
+            else if (response.statusCode >= 400) {
+                console.error('HTTP Error: ' + response.statusCode + ' - ' + response.statusMessage + '\n' + body);
+                
+            }
+            else {
+                console.log('Done!')
+                console.log(body)
+            }
+        });
+    }
+}
