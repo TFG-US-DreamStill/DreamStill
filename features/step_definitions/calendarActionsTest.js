246fbd5b00967052bf16efee6b1df17c656ed7d3
juanrarodriguez18 <dragondr3w@gmail.com>
2017-01-31 11:20:36 +0100

    Añadido base para nuevo test sobre el calendario

---
+++ features/step_definitions/calendarActionsTest.js
@@ -0,0 +1,59 @@
+var fs = require('fs');
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+
+var expect = chai.expect;
+
+module.exports = function () {
+    this.registerHandler('AfterScenario', function (event, callback) {
+            // clear localStorage browser.executeScript('window.localStorage.clear();');
+            callback();
+        });
+
+    this.Given(/^I am on the calendar page login as "([^"]*)"$/, function (arg1, callback) {
+        browser.ignoreSynchronization = true;
+        browser.get('/');
+        next();
+    });
+
+    this.Then(/^click to the calendar button "([^"]*)" two times$/, function (arg1, callback) {
+        if (text==="Previous"){
+            element.all(by.css('.purple-button')).get(0).click();
+            element.all(by.css('.purple-button')).get(0).click();
+            next();
+        }
+        if (text==="Next") {
+            element.all(by.css('.purple-button')).get(1).click();
+            element.all(by.css('.purple-button')).get(1).click();
+            next();
+        }
+        if (text==="Today"){
+            element.all(by.css('.purple-button')).get(2).click();
+            element.all(by.css('.purple-button')).get(2).click();
+            next();
+        }
+    });
+
+    this.Then(/^we must be on the "([^"]*)" month$/, function (arg1, callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.Then(/^click to the calendar button "([^"]*)" one time$/, function (arg1, callback) {
+        if (text==="Previous"){
+            element.all(by.css('.purple-button')).get(0).click();
+            next();
+        }
+        if (text==="Next") {
+            element.all(by.css('.purple-button')).get(1).click();
+            next();
+        }
+        if (text==="Today"){
+            element.all(by.css('.purple-button')).get(2).click();
+            next();
+        }
+    });
+
+};
--- features/test.feature
+++ features/test.feature
@@ -18,3 +18,17 @@ Feature: Check calendar events
     Given I am on the loginpage
     Then login to the Login Page that time as "juanra"
     Then there are must be "7" events on current month
+
+Feature: Check calendar actions
+  In order to check calendar actions
+  As a logged user
+  I want to be able to change the months views of the calendar
+
+  Scenario: Seeing calendar actions
+    Given I am on the calendar page login as "juanra"
+    Then click to the calendar button "Previous" two times 
+    And we must be on the "November 2016" month
+    Then click to the calendar button "Next" one time 
+    And we must be on the "December 2016" month
+    Then click to the calendar button "Today" one time 
+    And we must be on the "January 2017" month
