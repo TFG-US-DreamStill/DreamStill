f5ad6c1a33632582482aec5f6d79bb3297e3d604
juanrarodriguez18 <dragondr3w@gmail.com>
2017-02-05 12:21:24 +0100

    Añadidos tests de protractor al script de test

--- features/step_definitions/homePageTest.js
+++ features/step_definitions/homePageTest.js
@@ -31,7 +31,7 @@ module.exports = function() {
 
     this.Then(/the title should equal "([^"]*)"$/, function(text, next) {
             browser.waitForAngular();
-            browser.sleep(7000); 
+            browser.sleep(5000); 
             expect(browser.getTitle())
             .to.eventually.equal(text)
             .and.notify(next);
