0026dd2ba854ada91d7f13712c211b5a376145d3
juanrarodriguez18 <dragondr3w@gmail.com>
2017-03-08 20:07:02 +0100

    Añadida vista y componente para la página de alertas del usuario

---
+++ app/alerts.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'alerts',
+    styleUrls: ['alerts.component.css'],
+    templateUrl: 'alerts.component.html'
+})
+export class AlertsComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { CalendarComponent }    from './calendar.component';
 import { GraphsComponent }      from './graphs.component';
 import { ApisComponent }        from './apis.component';
 import { MorpheuzComponent }    from './morpheuz.component';
+import { AlertsComponent }       from './alerts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/calendar', pathMatch: 'full' },
   { path: 'calendar',   component: CalendarComponent },
   { path: 'graphs',     component: GraphsComponent },
   { path: 'apis',       component: ApisComponent},
-  { path: 'morpheuz',       component: MorpheuzComponent}
+  { path: 'morpheuz',       component: MorpheuzComponent},
+  { path: 'alerts',       component: AlertsComponent}
 ];
 
 @NgModule({
